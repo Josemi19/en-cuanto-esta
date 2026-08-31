02d6b95f346e6b9af7c384c40ec1bd92ee5996ef
Jose Morrone <josemmorrone@gmail.com>
2026-08-31 17:09:25 -0400

    ajuste titulo

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import "./globals.css";
 
 export const metadata: Metadata = {
   title: "En Cuanto Esta",
-  description: "Cotizaciones actualizadas de Venezuela."
+  description: "Tasas cambiarias actualizadas de Venezuela."
 };
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
