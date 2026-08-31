5501167bc42d3c3465e638e49e875bed09a9c30a
Jose Morrone <josemmorrone@gmail.com>
2026-08-31 17:04:34 -0400

    ajuste titulo

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import type { Metadata } from "next";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "En cuanto Esta",
+  title: "En Cuanto Esta",
   description: "Cotizaciones actualizadas de Venezuela."
 };
 
