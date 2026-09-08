b44001518cd7a77e9a6c581512eff6a70e6782a5
Jose Morrone <josemmorrone@gmail.com>
2026-09-08 13:52:27 -0400

    titulo anterior

--- app/page.jsx
+++ app/page.jsx
@@ -154,11 +154,7 @@ export default function Home() {
 
         <div className="intro">
           <p className="eyebrow">Venezuela</p>
-          <h1 id="page-title">En cuanto esta el dolar hoy</h1>
-          <p>
-            Consulta la tasa del dólar BCV, el valor en bolívares y otras tasas
-            cambiarias de Venezuela actualizadas.
-          </p>
+          <h1 id="page-title">Tasas del dia</h1>
         </div>
 
         <section
